af3123cbe811b616f4b1e697cc38185826862693
op07n <39182105+op07n@users.noreply.github.com>
2020-01-09 09:05:12 +0100

    Create streamlit_call.py

---
+++ streamlit_call.py
@@ -0,0 +1,5 @@
+from subprocess import Popen
+
+def load_jupyter_server_extension(nbapp):
+    """serve the streamlit app"""
+    Popen(["streamlit", "run", "mushroom_challenge/mushroom_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
