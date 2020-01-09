5b922e520dc1948e2d9a4d31b5a4be5447d803fb
op07n <39182105+op07n@users.noreply.github.com>
2020-01-09 10:22:39 +0100

    Update streamlit_call.py

--- .binder/streamlit_call.py
+++ .binder/streamlit_call.py
@@ -3,4 +3,5 @@ import os
 
 def load_jupyter_server_extension(nbapp):
     """serve the streamlit app"""
-    Popen(["streamlit", "run", "mushroom_challenge/mushroom_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"],cwd=path,env=dict(os.environ, PYTHONPATH="${PYTHONPATH}:/home/jovyan/mushroom_challenge/"))
+    os.chdir('mushroom_challenge/')
+    Popen(["streamlit", "run", "mushroom_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
