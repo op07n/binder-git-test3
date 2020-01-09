4773ed30d42ad87e8adb5f77a8e69f02bed29042
op07n <39182105+op07n@users.noreply.github.com>
2020-01-09 09:08:54 +0100

    Delete streamlit_call.py

--- streamlit_call.py
+++
@@ -1,5 +0,0 @@
-from subprocess import Popen
-
-def load_jupyter_server_extension(nbapp):
-    """serve the streamlit app"""
-    Popen(["streamlit", "run", "mushroom_challenge/mushroom_app.py", "--browser.serverAddress=0.0.0.0", "--server.enableCORS=False"])
